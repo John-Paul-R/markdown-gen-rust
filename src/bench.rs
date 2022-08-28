00b6395bc9fbf02703dc2656e21f058de0c315d1
John Paul Rutigliano <jp@jpcode.dev>
2022-08-28 15:12:08 -0400

    add tests

--- src/bench.rs
+++ src/bench.rs
@@ -5,7 +5,6 @@ mod tests {
     use test::Bencher;
     use crate::{exec};
 
-
     static INPUT_STRING: &str =
     "This is a test
 [I wonder What this does]!code{{
@@ -44,4 +43,51 @@ code block body lmao
         b.iter(|| exec(INPUT_STRING));
     }
 
+    #[test]
+    fn basic_valid() {
+        let input = "Testing pre text[clickable thing]{{summary body}}post text";
+        let expected_output =
+"Testing pre text<details>
+<summary><code>clickable thing</code></summary>
+
+
+summary body
+
+
+</details>post text";
+        let output = exec(input);
+        assert_eq!(expected_output, output);
+    }
+
+    #[test]
+    fn basic_code_valid() {
+        let input = "Testing pre text[clickable thing]!code{{summary body}}post text";
+        let expected_output =
+            "Testing pre text<details>
+<summary><code>clickable thing</code></summary>
+
+```
+summary body
+```
+
+</details>post text";
+        let output = exec(input);
+        assert_eq!(expected_output, output);
+    }
+
+    #[test]
+    fn basic_code_with_lang_valid() {
+        let input = "Testing pre text[clickable thing]!code.json{{summary body}}post text";
+        let expected_output =
+            "Testing pre text<details>
+<summary><code>clickable thing</code></summary>
+
+```json
+summary body
+```
+
+</details>post text";
+        let output = exec(input);
+        assert_eq!(expected_output, output);
+    }
 }
